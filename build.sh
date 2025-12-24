21c518490ec5f1edf050e5494fb5ba0f404d5a23
Arnaud M. <arnaud.choupinette@mailops.fr>
2025-12-24 13:44:52 +0100

    ajust build for my use case
    Signed-off-by: Arnaud M. <arnaud.choupinette@mailops.fr>

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ else
 fi
 
 docker buildx build \
-      --platform linux/amd64,linux/arm64 \
-      -t docker/getting-started:latest \
+      --platform linux/amd64 \
+      -t arnaudmorin/docker-tp:latest \
       $( (( $WILL_PUSH == 1 )) && printf %s '--push' ) .
